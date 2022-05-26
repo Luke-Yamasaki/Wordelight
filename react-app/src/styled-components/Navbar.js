f09220729142c24a9e51d9ffc003210343d825de
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-05-26 13:20:17 -0600

    Finished navbar

---
+++ react-app/src/styled-components/Navbar.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const Navbar = styled.nav`
+    width: 100vw;
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: ${props => props.theme === 'light' ? 'white' : '#191919'};
+`;
+
+export const NavList = styled.ul`
+    width: 98vw;
+    height: 60px;
+    margin: 0px;
+    padding: 0px 1vw; 0px 1vw;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    filter: ${props => props.theme === 'dark' ? 'invert()' : 'none'};
+`;
+
+export const NavItems = styled.li`
+    list-style: none;
+    padding: 0px;
+    margin: 0px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const IconBox = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+    gap: 10px;
+`;
+
+export const LogoBox = styled(IconBox)`
+    justify-content: center;
+    gap: 0px;
+`;
