78eae3e7a001bac9e47d336e30ed28a5f5f07bc9
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-05-26 12:47:02 -0600

    Colors placed in object now

--- react-app/utils/colors/colors.js
+++ react-app/utils/colors/colors.js
@@ -1,26 +1,26 @@
 export const colors = {
     "light": {
         "default": {
-            "correct": "",
-            "close": "",
-            "incorrect": ""
+            "correct": "#6AAA64",
+            "close": "#C9B458",
+            "incorrect": "#787C7E"
         },
         "contrast": {
-            "correct": "",
-            "close": "",
-            "incorrect": ""
+            "correct": "#F5793A",
+            "close": "#85C0F9",
+            "incorrect": "#787C7E"
         }
     },
     "dark": {
         "default": {
-            "correct": "",
-            "close": "",
-            "incorrect": ""
+            "correct": "#538D4E",
+            "close": "#B59F3B",
+            "incorrect": "#3A3A3C"
         },
         "contrast": {
-            "correct": "",
-            "close": "",
-            "incorrect": ""
+            "correct": "#BA5C2D",
+            "close": "#007A78",
+            "incorrect": "#3A3A3C"
         }
     }
 };
