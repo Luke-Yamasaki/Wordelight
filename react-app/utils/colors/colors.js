cf61d8fa957951f22acfbbd45505d2cf889e3db0
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-05-26 12:43:14 -0600

    Colors skeleton is good

---
+++ react-app/utils/colors/colors.js
@@ -0,0 +1,26 @@
+export const colors = {
+    "light": {
+        "default": {
+            "correct": "",
+            "close": "",
+            "incorrect": ""
+        },
+        "contrast": {
+            "correct": "",
+            "close": "",
+            "incorrect": ""
+        }
+    },
+    "dark": {
+        "default": {
+            "correct": "",
+            "close": "",
+            "incorrect": ""
+        },
+        "contrast": {
+            "correct": "",
+            "close": "",
+            "incorrect": ""
+        }
+    }
+};
